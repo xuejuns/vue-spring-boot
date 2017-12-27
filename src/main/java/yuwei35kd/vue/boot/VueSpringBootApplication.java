512af63e5755698d0f8c5dd8b810f6fc311142ef
yuwei35kd <yuwei35kd@163.com>
2017-12-27 16:16:21 +0800

    vue spring boot

---
+++ src/main/java/yuwei35kd/vue/boot/VueSpringBootApplication.java
@@ -0,0 +1,12 @@
+package yuwei35kd.vue.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringBootApplication.class, args);
+	}
+}
