c8c632aa2a80a7694c7366231b38db5d7ab63360
sxj <sxj@DESKTOP-MOMMIBQ>
2018-06-26 14:13:16 +0800

    33

--- src/main/java/yuwei35kd/vue/boot/VueSpringBootApplication.java
+++ src/main/java/yuwei35kd/vue/boot/VueSpringBootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VueSpringBootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ok");
 		SpringApplication.run(VueSpringBootApplication.class, args);
 	}
 }
