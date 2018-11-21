abd05a04adca689eec6ce89cb912923207119a86
xuejuns <xuejun_s@163.com>
2018-11-21 22:53:50 +0800

    commit

--- src/main/java/yuwei35kd/vue/boot/VueSpringBootApplication.java
+++ src/main/java/yuwei35kd/vue/boot/VueSpringBootApplication.java
@@ -3,6 +3,7 @@ package yuwei35kd.vue.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class VueSpringBootApplication {
 
